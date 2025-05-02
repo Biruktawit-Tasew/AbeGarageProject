9edab356bb173863854a6b42f00d47e9b087d8dc
Biruktawit-Tasew <birukti.tasew@gmail.com>
2025-05-02 14:25:36 -0300

    resource and adjustment on App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// Import the BrowserRouter 
+import { BrowserRouter } from 'react-router';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+    </BrowserRouter>
+
+  </StrictMode>
+);
