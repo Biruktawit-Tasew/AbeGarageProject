b75416cd169c37e212470015f91c4b326dba3fc5
Niyo-11 <getachewyonas59@gmail.com>
2025-05-09 04:00:24 +0200

    4:00am Friday

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -7,9 +7,6 @@ const ApiUrl = import.meta.env.VITE_API_URL;
 // create Z axios baseUrl
 const axiosBase = axios.create({
 	baseURL: `${ApiUrl}`,
-	headers: {
-		"Content-Type": "application/json",
-	},
 });
 
 // export Z axios baseUrl
